bf677782c941ec15a8fab8b4856c727f737b256f
ZicongCheung <1149394219@qq.com>
2024-07-11 11:52:30 +0800

    GetBonds-GitHub/THS

--- stock/bond/4.同花顺债券信息爬取.py
+++ stock/bond/4.同花顺债券信息爬取.py
@@ -1,18 +1,29 @@
 import requests
 import json
 
+# 获取数据
 url = 'https://data.10jqka.com.cn/ipo/kzz/'
 headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36 SE 2.X MetaSr 1.0'}
 response = requests.get(url, headers=headers)
 data = json.loads(response.text)
+
 # 获取list中前10组数据
 bond_list = data['list']
 latest_bonds = bond_list[:10]
-# 提取bond_code, bond_name, plan_total, success_rate, number
+
+# 初始化存储每组数据的列表
+all_bonds = []
+
+# 提取每组数据中的变量
 for bond in latest_bonds:
-    bond_code = bond['bond_code']
-    bond_name = bond['bond_name']
-    plan_total = bond['plan_total']
-    success_rate = bond['success_rate']
-    number = bond['number']
-    print(f"bond_code: {bond_code}, bond_name: {bond_name}, plan_total: {plan_total}, success_rate: {success_rate}, number: {number}")
+    bond_data = [
+        bond.get('bond_name', ''),
+        bond.get('plan_total', ''),
+        bond.get('success_rate', ''),
+        bond.get('number', '').replace('\r\n', '')
+    ]
+    all_bonds.append(bond_data)
+
+# 输出结果
+for bond in all_bonds:
+    print(bond)
