8e8199e3aa6e07c6e82fb81c4964e75485bff0d4
ZicongCheung <1149394219@qq.com>
2024-10-28 18:03:19 +0800

    更新 15.天天基金基金数据爬取.py

--- ZhangInvest/15.天天基金基金数据爬取.py
+++ ZhangInvest/15.天天基金基金数据爬取.py
@@ -12,4 +12,5 @@ fund_nav_date = fund_data['jzrq']
 fund_nav = fund_data['dwjz']
 fund_estimated_nav = fund_data['gsz']
 fund_estimated_growth_rate = fund_data['gszzl']
-print(f'基金名称：{fund_name}，净值日期：{fund_nav_date}，单位净值：{fund_nav}，估算净值：{fund_estimated_nav}，估算增长率：{fund_estimated_growth_rate}')
+fund_estimated_nav_date = fund_data['gztime']
+print(f'基金名称：{fund_name}，净值日期：{fund_nav_date}，单位净值：{fund_nav}，估算净值：{fund_estimated_nav}，估算增长率：{fund_estimated_growth_rate}，估算净值日期：{fund_estimated_nav_date}')
